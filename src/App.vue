e92150f72f9d728264421da1c5ffafd5c0f57871
Juan Vera <juanfrvera.work@gmail.com>
2025-03-26 14:09:44 -0300

    feat: catalog creation

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CatalogCreator from './components/CatalogCreator.vue'
+</script>
+
+<template>
+  <header>Mini Orders</header>
+
+  <main>
+    <CatalogCreator></CatalogCreator>
+
+    <label>WhatsApp number</label>
+    <input />
+    <button>Publish catalog</button>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/CatalogCreator.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+
+const items: Ref<Array<{ id: string; name: string; price: number; description: string }>> = ref([])
+
+function createNewItemRow() {
+  items.value.unshift({ id: crypto.randomUUID(), name: '', price: 0, description: '' })
+}
+</script>
+
+<template>
+  Hi, please add your catalog
+  <button @click="createNewItemRow()">Add Item</button>
+  <ol>
+    <li v-for="item in items" :key="item.id">
+      <label>Name</label>
+      <input v-model="item.name" />
+
+      <label>Price</label>
+      <input v-model="item.price" />
+
+      <label>Description</label>
+      <input v-model="item.description" />
+    </li>
+  </ol>
+</template>
+
+<style scoped></style>
