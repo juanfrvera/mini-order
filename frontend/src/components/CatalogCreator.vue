5ad7735a727fa1dcaff3c58c82e7a53688827355
Juan Vera <juanfrvera.work@gmail.com>
2025-04-01 09:19:25 -0300

    fix: styling and scripts

--- frontend/src/components/CatalogCreator.vue
+++ frontend/src/components/CatalogCreator.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { Item } from '@/typings/item';
+import type { Item } from '@/typings/item'
 import { ref, type Ref } from 'vue'
 
 defineExpose({ getItems })
@@ -16,9 +16,8 @@ function createNewItemRow() {
 </script>
 
 <template>
-  Hi, please add your catalog
   <button @click="createNewItemRow()">Add Item</button>
-  <ol>
+  <ol v-if="items.length">
     <li v-for="item in items" :key="item.id">
       <label>Name</label>
       <input v-model="item.name" />
@@ -30,6 +29,9 @@ function createNewItemRow() {
       <input v-model="item.description" />
     </li>
   </ol>
+  <div v-else>
+    <p>No items added yet. Click the button above to add an item.</p>
+  </div>
 </template>
 
 <style scoped></style>
--- frontend/src/pages/CreateCatalog.vue
+++ frontend/src/pages/CreateCatalog.vue
@@ -10,30 +10,41 @@ const phoneNumber = ref('')
 async function publishCatalog() {
   const items = catalogCreator.value?.getItems()
   if (items && items.length && shopUrl.value && phoneNumber.value) {
-      const response = await RequestService.publishCatalog(shopUrl.value, items, phoneNumber.value);
-      console.log(response);
-      const message = items
-        .map((item) => `${item.name} - ${item.price} - ${item.description}`)
-        .join('\n')
-      alert(`Catalog published with success! Message:\n${message}`)
+    const response = await RequestService.publishCatalog(shopUrl.value, items, phoneNumber.value)
+    console.log(response)
+    const message = items
+      .map((item) => `${item.name} - ${item.price} - ${item.description}`)
+      .join('\n')
+    alert(`Catalog published with success! Message:\n${message}`)
   } else {
-    alert('Please add items, shop URL and phone number');
+    alert('Please add items, shop URL and phone number')
   }
 }
 </script>
 
 <template>
-    <CatalogCreator ref="catalogCreator"></CatalogCreator>
+  Welcome to mini-orders, here you can create your catalog and publish it to your clients.
+  <p>To publish your catalog, please add the items, your shop URL and your WhatsApp number.</p>
+  <p>After publishing, you will be able to send the catalog to your clients.</p>
 
-    <div>
-      <label>Shop URL</label>
-      <input v-model="shopUrl" />
-    </div>
-    <div>
-      <label>Your WhatsApp number, where you want your clients to send the orders</label>
-      <input v-model="phoneNumber" />
-    </div>
-    <button @click="publishCatalog()">Publish catalog</button>
+  <h2>Items</h2>
+  <CatalogCreator ref="catalogCreator"></CatalogCreator>
+
+  <h2>Shop URL</h2>
+  <div>
+    <label>Shop URL</label>
+    <input v-model="shopUrl" />
+  </div>
+
+  <h2>WhatsApp phone number</h2>
+  <div>
+    <p>Please add your WhatsApp number, where you want your clients to send the orders</p>
+    <label>WhatsApp number</label>
+    <input v-model="phoneNumber" />
+  </div>
+
+  <h2>Publish</h2>
+  <button @click="publishCatalog()">Publish catalog</button>
 </template>
 
 <style scoped></style>
