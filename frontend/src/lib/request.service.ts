c7bf15632923d3438f5916eed30c64e429b85314
Juan Vera <juanfrvera.work@gmail.com>
2025-04-24 16:20:11 -0300

    feat: save catalog edit token

--- frontend/src/lib/request.service.ts
+++ frontend/src/lib/request.service.ts
@@ -2,13 +2,22 @@ import type { Item } from "@/typings/item";
 import { API_URL } from "./environment";
 
 export class RequestService {
-    public static publishCatalog(shopUrl: string, items: Item[], phone: string) {
-        return fetch(`${API_URL}/publishCatalog`, {
+    /** Token used to edit a created catalog */
+    private static catalogEditToken?: string;
+
+    public static async publishCatalog(shopUrl: string, items: Item[], phone: string) {
+        const response = await fetch(`${API_URL}/publishCatalog`, {
             method: 'POST', headers: {
                 'Content-Type': 'application/json' // Indicate JSON payload
             },
             body: JSON.stringify({ shopUrl, items, phone })
         });
+
+        const typed = await response.json() as { catalogEditToken: string, message: string };
+
+        this.catalogEditToken = typed.catalogEditToken;
+
+        return typed;
     }
 
     public static getCatalog(shopUrl: string) {
