4a3c233670c2fff627a5bc9d98753b84dcd56770
Juan Vera <juanfrvera.work@gmail.com>
2025-04-27 15:06:18 +0000

    feat: shop link actions

--- frontend/src/pages/ShopDashboard.vue
+++ frontend/src/pages/ShopDashboard.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
 import { adminStore } from '@/lib/store'
+
+// Store the current base URL of the application
+const baseUrl = window.location.origin
+const shopPublicUrl = `${baseUrl}/shop/${adminStore.shopUrl}`
+
+function copyPublicUrl() {
+    navigator.clipboard.writeText(shopPublicUrl).then(() => {
+        alert('Shop public URL copied to clipboard!')
+    }).catch(err => {
+        console.error('Failed to copy: ', err)
+        alert('Failed to copy the URL. Please try again.')
+    })
+}
+
 </script>
 
 <template>
 
     Welcome shop owner!
-    <div>The url of the shop is <b>{{ adminStore.shopUrl }}</b> and will be used to load the catalog</div>
+    <div>You can share the following link with your clients so they can see your catalog and order:</div>
+    <div class="public-url">
+        <span class="public-url__text">{{ shopPublicUrl }}</span>
+        <button @click="copyPublicUrl()" class="button-important public-url__copy">Copy link</button>
+        <a :href="shopPublicUrl" target="_blank"><button class="button-important public-url__view-as-client">View as
+                client</button></a>
+    </div>
 
     <div>
         This is your current catalog:
@@ -20,3 +40,15 @@ import { adminStore } from '@/lib/store'
         </div>
     </div>
 </template>
+
+<style scoped>
+.public-url {
+    padding: 20px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
