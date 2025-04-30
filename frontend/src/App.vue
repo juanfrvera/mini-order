e621f397b2dd2ea4b8d4ec61abdb5a8cc5596ef7
Juan Vera <juanfrvera.work@gmail.com>
2025-04-30 02:06:04 +0000

    feat: dark mode

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+
+function toggleDarkMode() {
+  const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
+  localStorage.setItem('darkMode', darkModeEnabled ? 'false' : 'true');
+  document.body.classList.toggle('dark-theme', !darkModeEnabled);
+}
+
 </script>
 
 <template>
@@ -6,6 +13,9 @@
     <header class="app-title">Mini Orders</header>
     <main>
       <RouterView />
+      <div class="config-bar">
+        <button @click="toggleDarkMode()">Toggle Dark Mode</button>
+      </div>
     </main>
   </div>
 </template>
@@ -21,5 +31,11 @@
   font-weight: bold;
   text-align: center;
   margin-bottom: 20px;
+}
+
+.config-bar {
+  position: fixed;
+  right: 4px;
+  top: 20px;
 }
 </style>
